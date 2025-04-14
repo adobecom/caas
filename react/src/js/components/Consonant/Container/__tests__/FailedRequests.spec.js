f7b72c16f62385070b73cfa283b127431f56453c
shkhan <shkhan@adobe.com>
2025-04-14 11:12:48 -0700

    ENB-7928: Linting Issues

--- react/src/js/components/Consonant/Container/__tests__/FailedRequests.spec.js
+++ react/src/js/components/Consonant/Container/__tests__/FailedRequests.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import { render, waitFor, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; // Import jest-dom for additional matchers
 import Container from '../Container'; // Adjust the import based on your project structure
 import config from '../../Testing/Mocks/config.json'; // Adjust the import based on your project structure
@@ -20,7 +19,6 @@ describe('Consonant/Container/Failed Requests', () => {
     test('should fallback to the fallback endpoint on fetch failure', async () => {
         const configToUse = { ...config, collection: { ...config.collection, endpoint: 'https://www.somedomain.com/some-test-api.json', fallbackEndpoint: 'https://www.somedomain.com/some-fallback-api.json' } };
 
-        // Mock fetch to fail initially and succeed on fallback
         global.fetch
             .mockImplementationOnce(() => Promise.reject(new Error('Network error')))
             .mockImplementationOnce(() => Promise.resolve({
@@ -52,7 +50,6 @@ describe('Consonant/Container/Failed Requests', () => {
     test('should succeed on subsequent fetch after initial failure', async () => {
         const configToUse = { ...config, collection: { ...config.collection, endpoint: 'https://www.somedomain.com/some-test-api.json', fallbackEndpoint: 'https://www.somedomain.com/some-fallback-api.json' } };
 
-        // Mock fetch to fail initially and succeed on retry
         global.fetch
             .mockImplementationOnce(() => Promise.reject(new Error('Network error')))
             .mockImplementationOnce(() => Promise.resolve({
