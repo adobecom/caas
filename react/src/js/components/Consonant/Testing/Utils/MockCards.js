259a2b3118af2c7d4a8f5f7b41b1f2ecddae7291
sr8384856 <sr8384856@gmail.com>
2025-06-03 15:39:19 -0700

    Saving demo. View.jsx is invalid

--- react/src/js/components/Consonant/Testing/Utils/MockCards.js
+++ react/src/js/components/Consonant/Testing/Utils/MockCards.js
@@ -8,18 +8,18 @@ import { DEFAULT_PROPS_ONE_HALF } from '../Constants/Card';
 
 /* eslint-disable import/prefer-default-export */
 export const generateCards = (count) => {
-  const cards = [];
-  for (let i = 0; i < count; i++) {
-    cards.push({
-      ...DEFAULT_PROPS_ONE_HALF,
-      id: `mock-card-${i}`,
-      contentArea: {
-        ...DEFAULT_PROPS_ONE_HALF.contentArea,
-        title: `Mock Card ${i}`,
-        description: `This is a mock card description for card ${i}`,
-      },
-      ctaLink: `https://adobe.com/card-${i}`,
-    });
-  }
-  return cards;
+    const cards = [];
+    for (let i = 0; i < count; i++) {
+        cards.push({
+            ...DEFAULT_PROPS_ONE_HALF,
+            id: `mock-card-${i}`,
+            contentArea: {
+                ...DEFAULT_PROPS_ONE_HALF.contentArea,
+                title: `Mock Card ${i}`,
+                description: `This is a mock card description for card ${i}`,
+            },
+            ctaLink: `https://adobe.com/card-${i}`,
+        });
+    }
+    return cards;
 };
