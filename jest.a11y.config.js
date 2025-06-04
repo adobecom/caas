cfbe4a24d4ead6534f6fb07c2a44b06c41d32c37
sr8384856 <sr8384856@gmail.com>
2025-06-04 00:15:29 -0700

    Final commit. Should be done with this

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -3,11 +3,11 @@ const baseConfig = require('./jest.config');
 
 module.exports = {
   ...baseConfig,
-  // Disable coverage reporting for a11y tests
-  collectCoverage: false,
-  coverageDirectory: undefined,
-  collectCoverageFrom: undefined,
-  coverageThreshold: undefined,
+  // Enable coverage reporting for a11y tests (no thresholds enforced)
+  collectCoverage: true,
+  coverageDirectory: 'coverage/a11y',
+  collectCoverageFrom: baseConfig.collectCoverageFrom,
+  coverageThreshold: {},
   // Only match *.a11y.test.js and *.a11y.test.jsx files
   testMatch: ['**/*.a11y.test.js', '**/*.a11y.test.jsx'],
 };
