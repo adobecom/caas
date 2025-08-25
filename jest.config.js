f02c85538e5b88f791d6631b9701a5267b05f344
sr8384856 <sr8384856@gmail.com>
2025-08-25 15:53:34 -0700

    attempt to fix ci/cd

--- jest.config.js
+++ jest.config.js
@@ -34,7 +34,7 @@ module.exports = {
     // An array of file extensions your modules use
     moduleFileExtensions: ['js', 'json', 'jsx'],
 
-    setupFiles: ['<rootDir>/enzyme.config.js'],
+    setupFilesAfterEnv: ['<rootDir>/enzyme.config.js'],
 
     // The test environment that will be used for testing
     testEnvironment: 'jsdom',
