563cd95e5a1ae78a466f3029cf7092c4d842e0cc
Sanjay Rai <sanrai@adobe.com>
2025-12-09 15:04:00 -0800

    Merge pull request #363 from adobecom/multi-level-filtering
    feat(mwpw-152657): support filter subset [Q1 FY26][BACOM]

--- react/src/js/components/Consonant/Filters/Top/Panel.jsx
+++ react/src/js/components/Consonant/Filters/Top/Panel.jsx
@@ -35,6 +35,8 @@ const filtersPanelTopType = {
     sortComponent: node.isRequired,
     windowWidth: number.isRequired,
     onFilterClick: func.isRequired,
+    // eslint-disable-next-line react/require-default-props
+    onCategoryToggle: func,
     onShowAllClick: func.isRequired,
     searchComponent: node.isRequired,
     searchQuery: string,
@@ -80,6 +82,7 @@ const FiltersPanelTop = (props) => {
         filters,
         resQty,
         onCheckboxClick,
+        onCategoryToggle,
         onFilterClick,
         onClearAllFilters,
         onClearFilterItems,
@@ -286,6 +289,7 @@ const FiltersPanelTop = (props) => {
                                 id={filter.id}
                                 isOpened={filter.opened}
                                 onCheck={onCheckboxClick}
+                                onCategoryToggle={(categoryId) => onCategoryToggle && onCategoryToggle(filter.id, categoryId)}
                                 onClick={onFilterClick}
                                 onClearAll={onClearFilterItems}
                                 clearFilterText={clearFilterText}
