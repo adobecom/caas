a84be980ef48300c5f940a416a199d2f877727bd
Carlos Miqueo <cmiqueo@adobe.com>
2026-03-05 16:07:03 -0800

    latest css updates

--- react/src/js/components/Consonant/Cards/Card.jsx
+++ react/src/js/components/Consonant/Cards/Card.jsx
@@ -203,6 +203,7 @@ const Card = (props) => {
     const bladeCard = getConfig('collection', 'bladeCard');
     const useCenterVideoPlay = getConfig('collection', 'useCenterVideoPlay');
     const searchEnabled = getConfig('search', 'enabled');
+    const editorialOpenVariant = getConfig('collection', 'editorialOpenVariant') || false;
 
     /**
      * Class name for the card:
@@ -213,6 +214,7 @@ const Card = (props) => {
         'consonant-Card': true,
         'consonant-u-noBorders': !renderBorder,
         'consonant-hide-cta': hideCTA,
+        'consonant-editorial--open': editorialOpenVariant,
     });
 
     /**
@@ -352,8 +354,9 @@ const Card = (props) => {
     const isFull = cardStyle === 'full-card';
     const isIcon = cardStyle === 'icon-card';
     const isNews = cardStyle === 'news-card';
-
+    const isEditorial = cardStyle === 'editorial-card';
     const isBlade = cardStyle === 'blade-card';
+    const isBlog = cardStyle === 'blog-card';
     const bladeVariant = isBlade
         ? [
             bladeCard.reverse ? 'reverse' : '',
@@ -371,11 +374,11 @@ const Card = (props) => {
     const showBadge = (isOneHalf || isThreeFourths || isFull) && (fromDexter || showCardBadges);
     const showLogo = isOneHalf || isThreeFourths || isFull || isText
         || (isHalfHeight && showCardBadges);
-    const showLabel = !isProduct && !isText;
+    const showLabel = !isProduct && !isText && !isEditorial || (isEditorial && editorialOpenVariant);
     const showVideoButton = !isProduct && !isText && !isIcon;
     const videoButtonStyle = useCenterVideoPlay && !isHalfHeight ? "center" : "";
     const showText = !isHalfHeight && !isFull && !isNews && !isHorizontal;
-    const showFooter = isOneHalf || isProduct || isText || isNews || isBlade;
+    const showFooter = isOneHalf || isProduct || isText || isNews || isBlade || isEditorial || isBlog;
     const showFooterLeft = !isProduct;
     const showFooterCenter = !isProduct && !altCtaUsed;
     let hideBanner = false;
@@ -623,7 +626,7 @@ const Card = (props) => {
                     )
                 ) }
                 {showFooter &&
-                !hideCTA &&
+                (!hideCTA || isBlog) &&
                 footer.map(footerItem => (
                     <CardFooter
                         divider={renderDivider || footerItem.divider}
@@ -634,13 +637,16 @@ const Card = (props) => {
                         center={showFooterCenter ? extendFooterData(footerItem.center) : []}
                         right={extendFooterData(footerItem.right)}
                         altRight={altCtaUsed ? extendFooterData(footerItem.altCta) : []}
+                        cardDate={new Date(cardDate)}
                         startDate={startDate}
                         endDate={endDate}
                         cardStyle={cardStyle}
                         onFocus={onFocus}
                         title={title}
                         tabIndex={tabIndex}
-                        renderOverlay={renderOverlay} />
+                        renderOverlay={renderOverlay}
+                        hideCTA={hideCTA}
+                        isBlog={isBlog} />
                 ))}
                 {(isThreeFourths || isDoubleWide || isFull)
                     && !renderOverlay
