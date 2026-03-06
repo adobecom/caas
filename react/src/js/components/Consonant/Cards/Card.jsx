7f1b173f86d3c5cac48bae807a85ced2b631e69d
Carlos Miqueo <cmiqueo@adobe.com>
2026-03-06 00:05:51 -0800

    server: merges editorial-card code

--- react/src/js/components/Consonant/Cards/Card.jsx
+++ react/src/js/components/Consonant/Cards/Card.jsx
@@ -355,6 +355,7 @@ const Card = (props) => {
     const isIcon = cardStyle === 'icon-card';
     const isNews = cardStyle === 'news-card';
     const isBlade = cardStyle === 'blade-card';
+    const isEditorial = cardStyle === 'editorial-card';
     const isBlog = cardStyle === 'blog-card';
     const bladeVariant = isBlade
         ? [
@@ -377,7 +378,7 @@ const Card = (props) => {
     const showVideoButton = !isProduct && !isText && !isIcon;
     const videoButtonStyle = useCenterVideoPlay && !isHalfHeight ? "center" : "";
     const showText = !isHalfHeight && !isFull && !isNews && !isHorizontal;
-    const showFooter = isOneHalf || isProduct || isText || isNews || isBlade || isBlog;
+    const showFooter = isOneHalf || isProduct || isText || isNews || isBlade || isEditorial || isBlog;
     const showFooterLeft = !isProduct;
     const showFooterCenter = !isProduct && !altCtaUsed;
     let hideBanner = false;
