8ac8cd0d7ab9cb1736b34264a5272fc13549086b
cmiqueo <64917520+cmiqueo@users.noreply.github.com>
2026-03-30 15:59:58 -0700

    feat(mwpw-190735): splits Card.jsx into different files for each card style (#432)
    * Initial commit
    
    * build: adds CardContext file
    
    * build: adds props validations
    
    * build: updates tests

---
+++ react/src/js/components/Consonant/Cards/HorizontalCard.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useCardData } from './CardContext';
+import CardHeader from './CardHeader/CardHeader';
+import CardContent from './CardContent/CardContent';
+import LinkBlocker from './LinkBlocker/LinkBlocker';
+
+const HorizontalCard = () => {
+    const {
+        id, lh, cardClassName,
+        optimizedImage, altText,
+        hasBanner, disableBanners,
+        bannerBackgroundColor, bannerFontColor, bannerIcon, bannerDescription,
+        videoURL, videoURLToUse, gateVideo, useCenterVideoPlay,
+        detailText,
+        highlightedTitle, title, headingAria, headingLevel,
+        parseMarkDown,
+        linkBlockerTarget, overlay, ctaText,
+        onFocus, tabIndex, ariaHidden,
+    } = useCardData();
+
+    return (
+        <li
+            daa-lh={lh}
+            className={`horizontal-card ${cardClassName}`}
+            data-testid="consonant-Card"
+            id={id}>
+            <CardHeader
+                image={optimizedImage}
+                altText={altText}
+                isIcon={false}
+                showBanner={hasBanner && !disableBanners}
+                bannerBackgroundColor={bannerBackgroundColor}
+                bannerFontColor={bannerFontColor}
+                bannerIcon={bannerIcon}
+                bannerDescription={bannerDescription}
+                showBadge={false}
+                showVideoButton
+                videoURL={videoURL}
+                videoURLToUse={videoURLToUse}
+                gateVideo={gateVideo}
+                title={title}
+                onFocus={onFocus}
+                tabIndex={tabIndex}
+                videoButtonClassName={`consonant-Card-videoIco ${useCenterVideoPlay ? 'center' : ''}`}
+                showLogo={false}
+                showIconLogo={false} />
+            <div className="consonant-Card-content">
+                <CardContent
+                    showLabel
+                    detailText={detailText}
+                    showIconAlt={false}
+                    isTitleOnly
+                    highlightedTitle={highlightedTitle}
+                    title={title}
+                    headingAria={headingAria}
+                    headingLevel={headingLevel}
+                    parseMarkDown={parseMarkDown}
+                    showText={false} />
+            </div>
+            <LinkBlocker
+                target={linkBlockerTarget}
+                link={overlay}
+                title={title}
+                getsFocus
+                ariaHidden={ariaHidden}
+                tabIndex={ariaHidden ? -1 : 0}
+                daa={ctaText} />
+        </li>
+    );
+};
+
+export default HorizontalCard;
